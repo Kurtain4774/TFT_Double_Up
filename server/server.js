998f56d4226d0adc0b3b95976e2061124e684444
Kurtis <pandabear4774@gmail.com>
2024-03-10 11:21:59 -0600

    Backend V0.01
    Successfully connected to riot api using summoner username and region

--- server/server.js
+++ server/server.js
@@ -1,18 +1,45 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/player", (req, res) => {
-    username = req.query.username;
-    region = req.query.region;
-    
-    res.json({"data": [{username}, {region}, "userThree", "userFour"]})
-})
+  username = req.query.username;
+  region = req.query.region;
 
-app.listen(3001,() => {
-    console.log("Server started at port 3001")
-})
+  //console.log(username + " " + region + " " + process.env.RIOT_API_KEY);
+
+  fetch(
+    "https://" +
+      region +
+      ".api.riotgames.com/tft/summoner/v1/summoners/by-name/" +
+      username +
+      "?api_key=" +
+      process.env.RIOT_API_KEY,
+    {
+      method: "GET",
+      mode: "cors",
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log("Printing Data:")
+      //console.log(data);
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send({
+        message: "Could not find summoner data",
+      });
+    });
+
+});
+
+app.listen(3001, () => {
+  console.log("Server started at port 3001");
+});
