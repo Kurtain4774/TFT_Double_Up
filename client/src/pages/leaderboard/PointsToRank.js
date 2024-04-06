31d071a0594e04ad75749b9c1cd31c19bdd5716a
Kurtis <kmquant@yahoo.com>
2024-04-06 11:05:38 -0600

    Got images to work on the leaderboard page

--- client/src/pages/leaderboard/PointsToRank.js
+++ client/src/pages/leaderboard/PointsToRank.js
@@ -1,17 +1,20 @@
+import imgChal from '../../images/ranks/TFT_Regalia_Challenger.png';
+import imgGM from '../../images/ranks/TFT_Regalia_GrandMaster.png';
+import imgMaster from '../../images/ranks/TFT_Regalia_Master.png';
+
 function LeaguePointsComponent({ leaguePoints }) {
   let imageSource;
   
   if (leaguePoints > 500) {
-    imageSource = '../../images/ranks/TFT_Regalia_Challenger.png';
+    imageSource = imgChal;
   } else if (leaguePoints > 200) {
-    imageSource = '../../images/ranks/TFT_Regalia_GrandMaster.png';
+    imageSource = imgGM;
   } else {
-    imageSource = '../../images/ranks/TFT_Regalia_Master.png';
+    imageSource = imgMaster;
   }
 
   return (
     <td>
-      
       <img src={imageSource} alt="League Icon" />
       {leaguePoints} LP
     </td>
